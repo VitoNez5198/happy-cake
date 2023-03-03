61457d7d71d051175f55915c0e553a13aec1957c
VitoNez5198 <113071024+VitoNez5198@users.noreply.github.com>
2023-03-03 00:27:50 -0300

    basename

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Footer from './components/Footer';
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <BrowserRouter basename="/home">
         <Navbar />
         <Routes>
           <Route path='/' element={<Home/>}/>
